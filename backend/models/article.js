f2994841e8de7a81482a19c9a65a8b63ccea8094
Tom lima <tom@tomlima.me>
2023-07-29 10:25:04 -0300

    Add support to axios cache interceptor

--- backend/models/article.js
+++ backend/models/article.js
@@ -1,12 +1,12 @@
-const axios = require('axios')
+const Axios = require('axios')
+const { setupCache } = require('axios-cache-interceptor');
+const axios = setupCache(Axios); 
+
 require('dotenv').config()
 
 module.exports = {
   getAll: (query, page) => {
     return new Promise((resolve, reject) => {
-      console.log(
-        `https://newsapi.org/v2/everything?q=${query}&pageSize=20&page=${page}&apiKey=${process.env.API_KEY}`
-      )
       axios
         .get(
           `https://newsapi.org/v2/everything?q=${query}&pageSize=20&page=${page}&apiKey=${process.env.API_KEY}`
